8efd054c3008dddd4976fca9861ff5b9dcabe2d4
Guilherme Kunsch <suporte@iconexlog.com.br>
2023-12-15 16:41:28 -0300

    adicionando segunda tela

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  if (email === "guilherme.kunsch@outlook.com" && senha === "080616") {
+    window.location.href = "cliente.html";
+  } else {
+    alert("E-mail ou senha incorretos. Tente novamente. ");
+  }
+});
